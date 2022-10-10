21aea6339c73667023344cbab20155388952a4f9
Damien Rice <44934900+Y2Kmeltdown@users.noreply.github.com>
2022-10-10 15:43:11 +1100

    fixed bracket

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ def compute_ISI_CV(spks:[])->[]:
     # [x for ind,x in enumerate(spks)]
     ISI_CV = np.asarray([np.std(neuron) / np.mean(neuron) for neuron in ISIs])
     st.markdown(ISI_CV)
-    st.markdown(average(ISI_CV)
+    st.markdown(average(ISI_CV))
     return ISI_CV
     # return a vector of scalars: ISI_CV
 
